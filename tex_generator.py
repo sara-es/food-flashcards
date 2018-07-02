cd20cf1db7d49445c858403720dcec3b3c6e832e
Joseph Fennell <jtfennell@gmail.com>
2018-07-02 18:18:33 +0100

    added tex generator placehoder python script

--- tex_generator.py
+++ tex_generator.py
@@ -4,5 +4,10 @@
 Created on Mon Jul  2 15:57:59 2018
 
 @author: joefennell
+
+This script takes an input csv and image collection
+and returns a latex formated text snippet which can be
+used to populate a flashcard
+
 """
 
