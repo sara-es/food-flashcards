83796f572a20b30cebe8cddbd9ad9675bfef54df
sara-es <summerton.sara@gmail.com>
2018-07-03 12:29:54 +0100

    Made tex_generator.py which will take cards.csv and write to catalogue.tex. Added some images. Latex compiles (for me at least).

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Python script for generating nice looking flashcards for 'Top Trump' style game
 
 ## Current TO-DO:
-- write the tex_generator.py script to scrape the food database and write a file similar to latex/catalogue.tex which contains the 5 parameters and a path to an image
+- ~~write the tex_generator.py script to scrape the food database and write a file similar to latex/catalogue.tex which contains the 5 parameters and a path to an image~~ sara-es 02.07
+- get suitable output for food database for cards.csv
 - compile an image library for all included foods
 - produce final printable stickers
 - if time, improve the look of the latex overwrite
--- tex_generator.py
+++ tex_generator.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Jul  2 15:57:59 2018
 
-@author: joefennell
+@author: sara-es
 
 This script takes an input csv and image collection
 and returns a latex formated text snippet which can be
@@ -11,3 +11,22 @@ used to populate a flashcard
 
 """
 
+import csv
+with open('cards.csv', newline='') as csvfile:
+    creader = csv.reader(csvfile, delimiter=',')
+    catalogue = open('latex/catalogue.tex', 'w')
+    for row in creader:
+        #sorry for the hardcoding hackjob
+        name = row[2].title()
+        img = row[2].replace(' ', '-').lower()
+        carbon = row[1].split('.',1)[0]
+        cals = row[3].split('.',1)[0]
+        prot = row[4].split('.',1)[0]
+        fib = row[5].split('.',1)[0]
+        sugar = row[6].split('.',1)[0]
+        fat = row[7].split('.',1)[0]
+        catalogue.write('\card{{{}}}{{{}}}{{{}}}{{{}}}{{{}}}{{{}}}{{images/{}.png}}\n'.format(
+            name, carbon, cals, fat, sugar, fib, img))
+        
+        #if I export data properly
+        #print('\card{{{}}}{{{}}}{{{}}}{{{}}}{{{}}}{{{}}}'.format(row[i] for i in row[1:6], img)
